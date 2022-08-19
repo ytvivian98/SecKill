6d1e180a84c0c4612512ac510ed71bac578a1e89
ytt <1401256003@qq.com>
2022-08-19 10:40:19 +0800

    秒杀倒计时的实现
    有一个坑就是秒杀结束后不会显示结束。。

--- src/main/java/com/seckill/controller/GoodsController.java
+++ src/main/java/com/seckill/controller/GoodsController.java
@@ -3,6 +3,7 @@ package com.seckill.controller;
 import com.seckill.pojo.User;
 import com.seckill.service.GoodsService;
 import com.seckill.service.UserService;
+import com.seckill.vo.GoodsVo;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  * 商品
@@ -59,7 +61,29 @@ public class GoodsController {
     @RequestMapping("/toDetail/{goodsId}")
     public String toDetail(Model model, User user, @PathVariable Long goodsId){
         model.addAttribute("user",user);
-        model.addAttribute("goods",goodsService.findGoodsVoByGoodsId(goodsId));
+        GoodsVo goodsVo = goodsService.findGoodsVoByGoodsId(goodsId);
+        Date startDate = goodsVo.getStartDate();
+        Date endDate = goodsVo.getEndDate();
+        Date nowDate = new Date();
+        //秒杀状态
+        int secKillStatus = 0;
+        int remainSeconds = 0;
+        //秒杀还未开始
+        if(nowDate.before(startDate)){
+            remainSeconds = (int)(startDate.getTime() - nowDate.getTime())/1000;
+        }
+        else if(nowDate.after(endDate)){
+            //秒杀已结束
+            secKillStatus = 2;
+            remainSeconds = -1;
+        }
+        else {
+            secKillStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("remainSeconds",remainSeconds);
+        model.addAttribute("secKillStatus",secKillStatus);
+        model.addAttribute("goods",goodsVo);
         return "goodsDetail";
     }
 }
